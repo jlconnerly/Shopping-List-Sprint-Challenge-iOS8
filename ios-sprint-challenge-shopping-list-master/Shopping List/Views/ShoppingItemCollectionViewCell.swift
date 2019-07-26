bc47020a13bc1f92f846e7e98b4284eb07ec0b52
Jake Connerly <jlconnerly@gmail.com>
2019-07-26 10:30:13 -0500

    Completed UI setup and respective files.

---
+++ ios-sprint-challenge-shopping-list-master/Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Jake Connerly on 7/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
